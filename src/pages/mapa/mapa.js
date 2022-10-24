afba8deb72eb37736e1435bdf46186adb19fd12e
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-10-23 22:59:26 -0300

    ultima commit

--- src/pages/mapa/mapa.js
+++ src/pages/mapa/mapa.js
@@ -1,12 +1,15 @@
 import { useMemo } from "react"
 import {GoogleMap} from '@react-google-maps/api'
 import '../mapa/mapa.css'
+import Marcadores from "./marcadores";
+
 
 
 import React, { useEffect, useRef } from 'react';
 
 function MapView() {
   const mapRef = useRef();
+ 
   useEffect(() => {
     new window.google.maps.Map(mapRef.current, {
       center: { lat: -30.033056, lng: -51.230000 },
@@ -15,7 +18,8 @@ function MapView() {
   }, []);
   
   return (
-  <div ref={mapRef} style={{ width: 1000, height: 1000 }}></div>
+  <div ref={mapRef} style={{ width: 1000, height: 1000 }}>
+  </div>
  
   )
 }
--- src/pages/mapa/marcadores.js
+++ src/pages/mapa/marcadores.js
@@ -1,40 +1,15 @@
+import { Marker } from "@react-google-maps/api";
 
-import { useMemo } from "react";
-import CriarProduto from "../Produto/criarProduto";
-import OverlayView from "../OverlayView";
-import MapView from '../mapa/mapa'
+const Marcadores = () => {
+    if(this.state.render === false){
+     this.Marcadores();
+    }
+    return this.state.locations.map((produto ,id)  => {
+     return <Marker
+       position={{lat: produto.lat, lng: produto.lng}}
+       key={id}
+      />
+    })
+   }
 
-interface CustomMarkerProps {
-  produto: produto.nomeProduto;
-  map?: google.maps.Map;
-}
-
-export default function CustomMarker({
-  produto,
-  map,
-}: CustomMarkerProps) {
-  const price = useMemo(() => {
-    return `$ ${hotel.ratesSummary.minPrice.replace(/\.(.*?\d*)/g, '')}`
-  }, [produto])
-  
-  return (
-    <>
-      {map && (
-        <OverlayView
-          position={{
-            lat: hotel.location.latitude as number,
-            lng: hotel.location.longitude as number,
-          }}
-          map={map}
-          styles={{
-            backgorundColor: 'DarkGray',
-            color: 'white',
-          }}
-        >
-          {/* use a button as the marker */}
-          <button onClick={handleClick}>{price}</button>
-        </OverlayView>
-      )}
-    </>
-  )
-}
+   export default Marcadores;
